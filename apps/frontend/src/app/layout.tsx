f52e1543bf172ca8dccace27daf70414040cb2d3
Nomena Nisiah <nix.nomena@gmail.com>
2026-08-20 10:20:53 +0300

    test pre commit message

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body className="min-h-screen bg-[#f3efe8] text-stone-900 antialiased">{children}</body>
+      <body className="min-h-screen bg-[#f3efe8] text-stone-900 antialiased ">{children}</body>
     </html>
   );
 }
